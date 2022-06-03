0b881f5382f2be7f14f4ca8b2fe8601f4c4a55bc
doosic <strou73@naver.com>
2022-06-03 20:08:17 +0900

    Initial commit

---
+++ src/main/java/com/fc_study/raisingpokemon/RaisingPokemonApplication.java
@@ -0,0 +1,13 @@
+package com.fc_study.raisingpokemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaisingPokemonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaisingPokemonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fc_study/raisingpokemon/RaisingPokemonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fc_study.raisingpokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaisingPokemonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
