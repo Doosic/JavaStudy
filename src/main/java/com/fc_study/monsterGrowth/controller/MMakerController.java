bc7f1dacbafb599c630f51562ae31d19d90cc7a5
doosic <strou73@naver.com>
2022-06-05 00:31:52 +0900

    만들어줄 클래스 및 어노테이션 정리 1차적으로는 끝

--- src/main/java/com/fc_study/monsterGrowth/controller/MMakerController.java
+++ src/main/java/com/fc_study/monsterGrowth/controller/MMakerController.java
@@ -1,7 +1,10 @@
 package com.fc_study.monsterGrowth.controller;
 
-import org.springframework.stereotype.Controller;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequiredArgsConstructor
 public class MMakerController {
+
 }
