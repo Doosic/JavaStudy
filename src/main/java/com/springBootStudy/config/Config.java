b4326474da25c3de3c123d74aa56e1b302ce1734
doosicEE <strou73@naver.com>
2022-06-16 08:59:08 +0900

    #5- SpringFramework 적용 및 빌드 배포
    SpringFramework 를 java 애플리케이션에 적용하고, 이후 webmvc 를 통해 war 파일을 만든 후 tomcat 에 빌드 배포하고 webService 를 띄워 크롬 브라우저에 확인.
    1. build.gradle에 webmvc 의존성 추가
    2. tomcat 다운로드 및 추가
    3. build.gradle에 plugins 으로 war 추가
    4. build 를 하여 build 파일을 만들고 빌드 할 war 파일을
    tomcat deployment 에 추가해준다.
    5. spring.io 홈페이지로 이동하여 dispatcherServlet 을 만드는 코드를 보고 사용한다.
    6. 톰켓을 스타트해 웹서버를 띄워본다.

--- src/main/java/com/springBootStudy/config/Config.java
+++ src/main/java/com/springBootStudy/config/Config.java
@@ -2,7 +2,9 @@ package com.springBootStudy.config;
 
 import org.springframework.context.annotation.ComponentScan;import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @ComponentScan("com.springBootStudy")
 @Configuration
 public class Config {
