4d68bc0ed776a314aa4d2bb308c2fb2fa89fe23e
doosicEE <strou73@naver.com>
2022-06-16 00:09:55 +0900

    #5- spring 적용해보기(실전)
    2개의 강의중 앞 강의 듣고 정리.
    Bean 을 주입해준다는걸 직접 만들어서 해보니
    더 와닿는다 그러나 아직은 조금 더 머리로 그림이 더 잘 그려질때까지 공부할 필요성을 느낀다. 핵심 개념들은 잘 알고 넘어가야 하니까.

--- src/main/java/com/springBootStudy/Main.java
+++ src/main/java/com/springBootStudy/Main.java
@@ -1,14 +1,27 @@
 package com.springBootStudy;
 
+import com.springBootStudy.config.Config;
 import com.springBootStudy.logic.JavaSort;
 import com.springBootStudy.logic.Sort;
+import com.springBootStudy.service.SortService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Sort<String> sort = new JavaSort<>();
+        ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+/*
+* Original
+* Sort<String> sort = new JavaSort<>();
+*
+* can't Not Sort Service. bring your own implementation
+* Sort<String> sort = context.getBean(Sort.class);
+*/
 
-        System.out.println("[result] " + sort.sort(Arrays.asList(args)));
+        SortService sortService = context.getBean(SortService.class);
+
+        System.out.println("[result] " + sortService.doSort(Arrays.asList(args)));
     }
 }
---
+++ src/main/java/com/springBootStudy/config/Config.java
@@ -0,0 +1,9 @@
+package com.springBootStudy.config;
+
+import org.springframework.context.annotation.ComponentScan;import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("com.springBootStudy")
+@Configuration
+public class Config {
+}
